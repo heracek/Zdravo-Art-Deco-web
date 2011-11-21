b4a152c60985dccf3ac1c0a9152c38211d5f052f
Tomas Horacek <tomas.horacek@gmail.com>
2011-11-22 00:54:50 +0100

    + support for lightbox for images ending -resized.(jpg|jpeg|png)

---
+++ content/media/js/zdravoartdeco.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('img:visible').filter(function(){
+    return /(-resized\.(jpg|jpeg|png)$)/.test(this.src);
+  }).each(function(){
+    var img_src = $(this).attr('src');
+    img_src = img_src.replace(/-resized\./, ".");
+    
+    var parent = $(this).wrap('<a href="' + img_src + '"></a>').parent()
+    $(parent).lightBox();
+  })
+});
